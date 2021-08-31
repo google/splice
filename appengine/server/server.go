38d4653b348918122357e94a8320c562e485d1ca
Matt LaPlante <mattl@google.com>
2021-08-31 13:38:54 -0700

    Use iota for counting server codes.
    PiperOrigin-RevId: 394075601

--- appengine/server/server.go
+++ appengine/server/server.go
@@ -21,36 +21,47 @@ package server
 // reasons for request and result rejections, as well as internal failures.
 type StatusCode int
 
-// Server Internal Status Messages. We use static values to ensure that messages
-// that may end up in the datastore do not have to be altered if a new const is added.
 const (
-	StatusSuccess            StatusCode = 0
-	StatusRequestUnreadable  StatusCode = 101
-	StatusJSONEmpty          StatusCode = 102
-	StatusJSONUmarshalError  StatusCode = 103
-	StatusJSONMarshalError   StatusCode = 104
-	StatusReqProcessingError StatusCode = 105
-	StatusInvalidCertError   StatusCode = 106
-	StatusInvalidGCEmeta     StatusCode = 107
+	// StatusSuccess indicates a successful operation.
+	StatusSuccess StatusCode = 0
+)
 
-	// Default validator messages
-	StatusRequestHostBlank     StatusCode = 201
-	StatusRequestHostLength    StatusCode = 202
-	StatusRequestClientIDBlank StatusCode = 203
-	StatusRequestResultReplay  StatusCode = 204
+// Server Internal Status Messages.
+const (
+	StatusRequestUnreadable StatusCode = iota + 101
+	StatusJSONEmpty
+	StatusJSONUmarshalError
+	StatusJSONMarshalError
+	StatusReqProcessingError
+	StatusInvalidCertError
+	StatusInvalidGCEmeta
+)
 
-	// Dependency validator messages
-	StatusDependencyValidationError StatusCode = 301
+// Default validator messages
+const (
+	StatusRequestHostBlank StatusCode = iota + 201
+	StatusRequestHostLength
+	StatusRequestClientIDBlank
+	StatusRequestResultReplay
+)
 
-	// Datastore status messages
-	StatusDatastoreClientCreateError StatusCode = 401
-	StatusDatastoreTxCreateError     StatusCode = 402
-	StatusDatastoreWriteError        StatusCode = 403
-	StatusDatastoreLookupError       StatusCode = 404
-	StatusDatastoreLookupNotFound    StatusCode = 405
-	StatusDatastoreUpdateError       StatusCode = 406
-	StatusDatastoreTxCommitError     StatusCode = 407
+// Dependency validator messages
+const (
+	StatusDependencyValidationError StatusCode = iota + 301
+)
 
-	// Pubsub status messages
-	StatusPubsubFailure StatusCode = 501
+// Datastore status messages
+const (
+	StatusDatastoreClientCreateError StatusCode = iota + 401
+	StatusDatastoreTxCreateError
+	StatusDatastoreWriteError
+	StatusDatastoreLookupError
+	StatusDatastoreLookupNotFound
+	StatusDatastoreUpdateError
+	StatusDatastoreTxCommitError
+)
+
+// Pubsub status messages
+const (
+	StatusPubsubFailure StatusCode = iota + 501
 )
